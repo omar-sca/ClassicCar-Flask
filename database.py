a22bb24e5bf9b860d323c95dad41cbbc00bf76f6
omar-sca <omar.sto00@gmail.com>
2023-12-21 10:56:55 -0300

    ClassicCar-Flask

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 from config import DevelopmentConfig
 
-from z_load_data import cargar_autos, cargar_usuarios, cargar_grupos
+#Carga de datos hardcodeada
+#from z_load_data import cargar_autos, cargar_usuarios, cargar_grupos
 
 engine = create_engine(DevelopmentConfig.SQLALCHEMY_ENGINE)
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -19,6 +20,6 @@ def init_db():
     import models
     Base.metadata.drop_all(bind=engine) # elimina todas la tablas de la base de datos
     Base.metadata.create_all(bind=engine)
-    cargar_autos(models.Auto, models.Reserva, db_session)
-    cargar_grupos(models.Grupo, db_session)
-    cargar_usuarios(models.Usuario, db_session)
+    #cargar_autos(models.Auto, models.Reserva, db_session)
+    #cargar_grupos(models.Grupo, db_session)
+    #cargar_usuarios(models.Usuario, db_session)
